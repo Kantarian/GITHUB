15b0998f9bdd3a9d20c07bf7c356a46597417735
Kantarian <72248382+Kantarian@users.noreply.github.com>
2020-11-11 13:03:38 +0200

    Update HT_1_5.py

--- HT_1/HT_1_5.py
+++ HT_1/HT_1_5.py
@@ -1,3 +1,7 @@
+#5. Write a script to convert decimal to hexadecimal
+        #Sample decimal number: 30, 4
+        #Expected output: 1e, 04
+
 while True:
     d = int(input('Input a decimal: '))
     h = format(d, '02x')

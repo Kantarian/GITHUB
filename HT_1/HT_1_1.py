b00c61eee104dd1a6216555ba95eabb8a58d7769
Kantarian <72248382+Kantarian@users.noreply.github.com>
2020-11-11 12:59:01 +0200

    Update HT_1_1.py

--- HT_1/HT_1_1.py
+++ HT_1/HT_1_1.py
@@ -1,3 +1,14 @@
+#1 .Write a script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+       # Sample data : 1, 5, 7, 23
+       # Output :
+       # List : [‘1', ' 5', ' 7', ' 23']
+       # Tuple : (‘1', ' 5', ' 7', ' 23')
+
+
+
+
+
+
 numbers = input('Input some comma seprated numbers: ')
 list = numbers.split(",")
 tuple = tuple(list)

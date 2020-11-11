621893aba73c8d3f4c7ecc541da6044c597a6cb1
Kantarian <72248382+Kantarian@users.noreply.github.com>
2020-11-11 13:04:36 +0200

    Update HT_1_6.py

--- HT_1/HT_1_6.py
+++ HT_1/HT_1_6.py
@@ -1,3 +1,9 @@
+#6. Write a script to check whether a specified value is contained in a group of values.
+        #Test Data :
+        #3 -> [1, 5, 8, 3] : True
+        #-1 -> (1, 5, 8, 3) : False
+
+
 def is_group_member(group_data, n):
     for value in group_data:
         if n == value:

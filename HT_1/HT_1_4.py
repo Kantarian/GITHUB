16104cea45a458b2fe7558d190f176cf8a50736c
Kantarian <72248382+Kantarian@users.noreply.github.com>
2020-11-11 12:24:46 +0200

    Add files via upload

---
+++ HT_1/HT_1_4.py
@@ -0,0 +1,9 @@
+strings = []
+string = input(str("Please write Start for enter your string: "))
+while string != "" or string != " ":
+    string = input(str("Please enter your string: "))
+     
+    if string is not string != "" or string != " ":
+        strings.append(string)
+        print(", ".join(strings))
+print(", ".join(strings))   
---
+++ HT_1/HT_1_5.py
@@ -0,0 +1,5 @@
+while True:
+    d = int(input('Input a decimal: '))
+    h = format(d, '02x')
+    print('Hexadecimal: ',h)
+    

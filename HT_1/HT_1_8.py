716f82dfd402c0258186ec0948a86c221682f945
Kantarian <72248382+Kantarian@users.noreply.github.com>
2020-11-11 13:05:31 +0200

    Update HT_1_8.py

--- HT_1/HT_1_8.py
+++ HT_1/HT_1_8.py
@@ -1,2 +1,7 @@
+#8. Write a script to replace last value of tuples in a list.
+        #Sample list: [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+
+
+
 l = [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
 print([t[:-1] + (100,) for t in l])
